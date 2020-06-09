af54184db03f3279bc8e61c34023af37ad426223
Wyatt Barnes <me@wyatt.email>
2020-06-08 23:47:13 -0500

    Working prototype

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"math"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
@@ -26,20 +27,40 @@ type invoiceEntry struct {
 	UnitCost float64 `json:"unit_cost"`
 }
 
+type invoiceRequestData struct {
+	From string `json:"from"`
+	To string `json:"to"`
+	Number float64 `json:"number"`
+	Items []invoiceEntry `json:"items"`
+	Notes string `json:"notes"`
+}
+
+const INVOICE_GENERATOR_URL = "https://invoice-generator.com"
+
+var sender string
+var receiver string
+var invoiceNumber float64
 var ratePerHour float64
+var note string
 
 func main() {
-	ratePerHourPtr := flag.Float64("r", 0, "Rate charged per hour")
+	senderPtr := flag.String("s", "Elliot Alderson", "Name of invoice sender")
+	receiverPtr := flag.String("r", "Allsafe Cybersecurity", "Name of invoice receiver")
+	invoiceNumberPtr := flag.Float64("n", 1, "Invoice number")
+	ratePerHourPtr := flag.Float64("ra", 0, "Rate charged per hour")
+	notePtr := flag.String("no", "Thank you for your business", "Additional note provided at the end of invoice")
 	filepathPtr := flag.String("f", "data.json", "Filepath to JSON file of Clockify entries")
-	// outputFileNamePtr := flag.String("o", "invoice.pdf", "Desired name for invoice PDF")
+	outputFileNamePtr := flag.String("o", "invoice.pdf", "Desired name for invoice PDF")
 	flag.Parse()
 
+	sender = *senderPtr
+	receiver = *receiverPtr
+	invoiceNumber = *invoiceNumberPtr
 	ratePerHour = *ratePerHourPtr
+	note = *notePtr
 
 	jsonFile, err := os.Open(*filepathPtr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
@@ -47,15 +68,25 @@ func main() {
 	var clockifyEntries []clockifyEntry
 	json.Unmarshal(byteValue, &clockifyEntries)
 
+	var invoiceEntries []invoiceEntry
+
 	for i := 0; i < len(clockifyEntries); i++ {
-		fmt.Println(buildInvoiceEntry(clockifyEntries[i]))
+		invoiceEntries = append(invoiceEntries, buildInvoiceEntry(clockifyEntries[i]))
 	}
+	
+	generateInvoice(buildRequestData(invoiceEntries), *outputFileNamePtr)
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
 
 func buildInvoiceEntry(entryData clockifyEntry) invoiceEntry {
 	invoiceEntryName := buildInvoiceEntryName(entryData.ClientName, entryData.ProjectName, entryData.Description)
 	invoiceQuantity := getInvoiceEntryQuantity(entryData.Duration)
-	return invoiceEntry{ Name: invoiceEntryName, Quantity: invoiceQuantity, UnitCost: ratePerHour }
+	return invoiceEntry{Name: invoiceEntryName, Quantity: invoiceQuantity, UnitCost: ratePerHour}
 }
 
 func buildInvoiceEntryName(clientName string, projectName string, description string) string {
@@ -69,33 +100,68 @@ func getInvoiceEntryQuantity(duration string) float64 {
 
 	re := regexp.MustCompile(`PT(\d+H)?(\d+M)?(\d+S)?`)
 	for _, match := range re.FindStringSubmatch(duration) {
-		if (strings.HasPrefix(match, "P")) {
+		if strings.HasPrefix(match, "P") {
 			continue
-		} else if (strings.HasSuffix(match, "H")) {
+		} else if strings.HasSuffix(match, "H") {
 			hours, _ = strconv.ParseFloat(strings.TrimSuffix(match, "H"), 32)
-		} else if (strings.HasSuffix(match, "M")) {
+		} else if strings.HasSuffix(match, "M") {
 			minutes, _ = strconv.ParseFloat(strings.TrimSuffix(match, "M"), 32)
-		} else if (strings.HasSuffix(match, "S")) {
+		} else if strings.HasSuffix(match, "S") {
 			seconds, _ = strconv.ParseFloat(strings.TrimSuffix(match, "S"), 32)
 		}
 	}
-	
+
 	return calculateTotalHours(hours, minutes, seconds)
 }
 
 func calculateTotalHours(numHours float64, numMinutes float64, numSeconds float64) float64 {
-	if (numSeconds >= 30) {
+	if numSeconds >= 30 {
 		numMinutes++
 	}
-	
-	return toFixed(numHours + (numMinutes / 60), 2)
+
+	return toFixed(numHours+(numMinutes/60), 2)
 }
 
 func round(num float64) int {
-    return int(num + math.Copysign(0.5, num))
+	return int(num + math.Copysign(0.5, num))
 }
 
 func toFixed(num float64, precision int) float64 {
-    output := math.Pow(10, float64(precision))
-    return float64(round(num * output)) / output
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func buildRequestData(invoiceEntries []invoiceEntry) []byte {
+	requestData := invoiceRequestData {From: sender, To: receiver, Number: invoiceNumber, Items: invoiceEntries, Notes: note}
+
+	bytes, err := json.Marshal(requestData)
+	check(err)
+	
+	return bytes
+}
+
+func generateInvoice(requestData []byte, outputFileName string) {
+	req, err := http.NewRequest("POST", INVOICE_GENERATOR_URL, bytes.NewBuffer(requestData))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	check(err)
+	defer resp.Body.Close()
+	
+	if (resp.Status == "200 OK") {
+		body, _ := ioutil.ReadAll(resp.Body)
+		createFile(body, outputFileName)
+	}
+}
+
+func createFile(data []byte, filePath string) {
+	f, err := os.Create(filePath)
+	defer f.Close()
+	check(err)
+	f.Write(data)
+	fmt.Println("Invoice generated successfully")
 }
