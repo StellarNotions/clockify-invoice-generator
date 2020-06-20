e303b007dcac0f4d7ea27ce9e41dfe68df6423c3
Wyatt Barnes <me@wyatt.email>
2020-06-20 17:34:11 -0500

    Add comments

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+// BUG(spacesailor24) calculateHours method does not match total time calculation done by Clockify.
+
 import (
 	"bytes"
 	"encoding/json"
@@ -14,6 +16,7 @@ import (
 	"strings"
 )
 
+// configObj used to populate PDF with non-standard values.
 type configObj struct {
 	Sender string `json:"sender"`
 	Receiver string `json:"receiver"`
@@ -28,6 +31,7 @@ type configObj struct {
 	OutPutFilePath string `json:"outputFilePath"`
 }
 
+// clockifyEntry needed values from Clockify's response data to construct invoiceEntry.
 type clockifyEntry struct {
 	Description string `json:"description"`
 	ProjectName string `json:"projectName"`
@@ -35,12 +39,14 @@ type clockifyEntry struct {
 	Duration    string `json:"duration"`
 }
 
+// invoiceEntry needed values to construct Clockify time entries.
 type invoiceEntry struct {
 	Name     string  `json:"name"`
 	Quantity float64 `json:"quantity"`
 	UnitCost float64 `json:"unit_cost"`
 }
 
+// invoiceRequestData data being sent to InvoiceGeneratorURL to generate PDF.
 type invoiceRequestData struct {
 	From string `json:"from"`
 	To string `json:"to"`
@@ -49,7 +55,8 @@ type invoiceRequestData struct {
 	Notes string `json:"notes"`
 }
 
-const INVOICE_GENERATOR_URL = "https://invoice-generator.com"
+// invoiceGeneratorURL URL invoiceRequestData is sent to, to generate PDF.
+const invoiceGeneratorURL = "https://invoice-generator.com"
 
 var config configObj
 var clockifyEntries []clockifyEntry
@@ -70,40 +77,52 @@ func main() {
 	generateInvoice(buildRequestData(invoiceEntries), config.OutPutFilePath)
 }
 
+// check if error is found, panics.
 func check(e error) {
     if e != nil {
         panic(e)
     }
 }
 
+// parseConfigFile reads filePath and unmarshals into config type object.
 func parseConfigFile(filePath string) {
-	jsonFile, err := os.Open(filePath)
-	check(err)
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, &config)
+	var bytesFile = getFileAsBytes(filePath)
+	json.Unmarshal(bytesFile, &config)
 }
 
+// parseDataFile reads filePath and unmarshals into clockifyEntries type object.
 func parseDataFile(filePath string) {
+	var bytesFile = getFileAsBytes(filePath)
+	json.Unmarshal(bytesFile, &clockifyEntries)
+}
+
+// getFileAsBytes reads filePath and returns bytes value.
+func getFileAsBytes(filePath string) []byte {
 	jsonFile, err := os.Open(filePath)
 	check(err)
 	defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, &clockifyEntries)
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	check(err)
+	return byteValue
 }
 
+// buildInvoiceEntry uses provided data to generate a single invoiceEntry that will be displayed in generated invoice.
 func buildInvoiceEntry(entryData clockifyEntry) invoiceEntry {
 	invoiceEntryName := buildInvoiceEntryName(entryData.ClientName, entryData.ProjectName, entryData.Description)
 	invoiceQuantity := getInvoiceEntryQuantity(entryData.Duration)
 	return invoiceEntry{Name: invoiceEntryName, Quantity: invoiceQuantity, UnitCost: config.RatePerHour}
 }
 
+// buildInvoiceEntryName returns formatted string used to represent what was worked on.
+// The format of the string is generic and can be changed to anything.
 func buildInvoiceEntryName(clientName string, projectName string, description string) string {
 	return fmt.Sprintf("Client Name: %s | Project Name: %s | Description: %s", clientName, projectName, description)
 }
 
+// getInvoiceEntryQuantity parses a Clockify formatted duration string (in the format of PT1H1M1S) into
+// each individual time segment.
+// Returns total amount of hours worked.
 func getInvoiceEntryQuantity(duration string) float64 {
 	var hours float64
 	var minutes float64
@@ -125,6 +144,8 @@ func getInvoiceEntryQuantity(duration string) float64 {
 	return calculateHours(hours, minutes, seconds)
 }
 
+// calculateHours uses provided data to calculate the amount of hours worked.
+// Rounded to nearest 1/10th of an hour.
 func calculateHours(numHours float64, numMinutes float64, numSeconds float64) float64 {
 	if numSeconds > 0 {
 		numMinutes++
@@ -132,15 +153,18 @@ func calculateHours(numHours float64, numMinutes float64, numSeconds float64) fl
 	return numHours + toFixed(numMinutes/60, 1)
 }
 
+// round Helper function to round float64s.
 func round(num float64) int {
 	return int(num + math.Copysign(0.5, num))
 }
 
+// toFixed Rounds a number to given number of decimals.
 func toFixed(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
 	return float64(round(num*output)) / output
 }
 
+// buildRequestData Builds need JSON object to submit to invoice generater.
 func buildRequestData(invoiceEntries []invoiceEntry) []byte {
 	requestData := invoiceRequestData {From: config.Sender, To: config.Receiver, Number: config.InvoiceNumber, Items: invoiceEntries, Notes: buildNotes()}
 
@@ -149,12 +173,14 @@ func buildRequestData(invoiceEntries []invoiceEntry) []byte {
 	return bytes
 }
 
+// buildNotes Uses configObj to build notes section of invoice.
 func buildNotes() string {
 	return fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n", config.Name, config.Email, config.Address, config.InvoicePeriod, config.Notes)
 }
 
+// generateInvoice Takes requestData and submits data to invoiceGeneratorURL and save PDF to outputFileName.
 func generateInvoice(requestData []byte, outputFileName string) {
-	req, err := http.NewRequest("POST", INVOICE_GENERATOR_URL, bytes.NewBuffer(requestData))
+	req, err := http.NewRequest("POST", invoiceGeneratorURL, bytes.NewBuffer(requestData))
 	if err != nil {
 		panic(err)
 	}
@@ -171,6 +197,7 @@ func generateInvoice(requestData []byte, outputFileName string) {
 	}
 }
 
+// createFile Write data to filePath.
 func createFile(data []byte, filePath string) {
 	f, err := os.Create(filePath)
 	defer f.Close()
